5d007a769999fad933e371073280b6b38f500016
Gary Wheeler <gkwheeler40@gmail.com>
2019-04-09 02:26:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzBuzz(number)
-  if number % 3 == 0 
+def fizzBuzz(Integer)
+  if Integer % 3 == 0 
     "Fizz"
-  elsif number % 5 == 0
+  elsif Integer % 5 == 0
     "Buzz"
-  elsif number % 3 == 0 && number % 5 == 0
+  elsif Integer % 3 == 0 && Integer % 5 == 0
     "FizzBuzz"
   else
-    number
+    Integer
   end
 end
