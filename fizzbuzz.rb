d4dd4151824e942448cbd1bddd1c02713880814e
Gary Wheeler <gkwheeler40@gmail.com>
2019-04-09 02:33:18 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzBuzz
+def fizzBuzz(Integer)
   if Integer % 3 == 0 
     "Fizz"
   elsif Integer % 5 == 0
