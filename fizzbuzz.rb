f797861d162470a5037cfb551072918d764eb650
Gary Wheeler <gkwheeler40@gmail.com>
2019-04-09 02:04:48 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz
-  if i % 3 == 0
+  if int % 3 == 0
     print "fizz"
 end
