e2cd0f3cfc23b4131eb598edb7785a4421108182
Gary Wheeler <gkwheeler40@gmail.com>
2019-04-09 02:49:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzBuzz("number.to_i")
+def fizzBuzz(int)
   if "number" % 3 == 0 
    print "Fizz"
   elsif Integer % 5 == 0
